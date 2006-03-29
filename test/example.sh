a6dfbc67afdd4e938c8026099b163420bb56b941
Martin Grunewald <sha1-9a467096b0966b12a20918c4a8bfa57c962386f8@cern.ch>
2006-03-29 08:19:34 +0000

    *** empty log message ***

--- test/example.sh
+++ test/example.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 echo "Getting test beam file (big!)"
-#wget http://cern.ch/jmans/cms/HTB_011609.root
+wget http://cern.ch/jmans/cms/HTB_011609.root
 
 #echo "Getting simulated file"
 #wget http://cmsdoc.cern.ch/cms/data/Validation/Simulation/pi100GeV_detsim_digi_pileup.50evt.root

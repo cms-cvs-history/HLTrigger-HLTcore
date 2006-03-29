74203139f72460e79bc07b2df216247092a768d3
Martin Grunewald <sha1-9a467096b0966b12a20918c4a8bfa57c962386f8@cern.ch>
2006-03-29 08:17:42 +0000

    *** empty log message ***

--- test/example.sh
+++ test/example.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
 
-cp $CMSSW_RELEASE_BASE/src/RecoJets/JetProducers/test/CaloTowers_HB_HO.root .
+echo "Getting test beam file (big!)"
+#wget http://cern.ch/jmans/cms/HTB_011609.root
+
+#echo "Getting simulated file"
+#wget http://cmsdoc.cern.ch/cms/data/Validation/Simulation/pi100GeV_detsim_digi_pileup.50evt.root
+
+echo "Making CaloTowers with cmsRun"
+
+cmsRun -p make_CaloTowers_HB_HO.cfg
+
+echo "Making Jets and running simple jet HLTs"
 
 cmsRun -p example.cfg

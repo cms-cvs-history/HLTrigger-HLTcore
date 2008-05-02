8d3c3d3e15f110e857baf16d223eefcea76cc9f9
cmsbuild <>
2008-05-02 12:13:29 +0000

    This commit was manufactured by cvs2git to create tag 'HLTrigger- HLTcore-V02-02-02'.
    Sprout from master 2008-05-02 12:13:28 UTC Martin Grunewald <sha1-9a467096b0966b12a20918c4a8bfa57c962386f8@cern.ch> 'AOD summary config changes'
    Cherrypick from master 2008-03-28 09:48:07 UTC Rick Wilkinson <sha1-197b93d87bf2e1eb4349df76c6ec25fd8f1f348e@cern.ch> 'adding new py configs':
        interface/HLTPrescaler.h
        plugins/HLTPrescaler.cc
        python/__init__.py
    Delete:
        ChangeLog
        python/triggerSummaryAnalyzerAOD_cfi.py
        python/triggerSummaryAnalyzerRAW_cfi.py

--- plugins/HLTPrescaler.cc
+++ plugins/HLTPrescaler.cc
@@ -1,11 +1,14 @@
-////////////////////////////////////////////////////////////////////////////////
-//
-// HLTPrescaler
-// ------------
-//
-//            04/25/2008 Philipp Schieferdecker <philipp.schieferdecker@cern.ch>
-////////////////////////////////////////////////////////////////////////////////
-
+/** \class HLTPrescaler
+ *
+ *  
+ *  See header file for documentation.
+ *
+ *  $Date: 2008/01/09 13:59:06 $
+ *  $Revision: 1.5 $
+ *
+ *  \author Martin Grunewald
+ *
+ */
 
 #include "HLTrigger/HLTcore/interface/HLTPrescaler.h"
 #include "DataFormats/HLTReco/interface/TriggerFilterObjectWithRefs.h"
@@ -14,65 +17,81 @@
 #include "FWCore/ServiceRegistry/interface/Service.h"
 #include "FWCore/MessageLogger/interface/MessageLogger.h"
 
-
-////////////////////////////////////////////////////////////////////////////////
-// construction/destruction
-////////////////////////////////////////////////////////////////////////////////
-
-//______________________________________________________________________________
-HLTPrescaler::HLTPrescaler(edm::ParameterSet const& iConfig)
-  : prescaleFactor_(1)
-  , eventCount_(0)
-  , acceptCount_(0)
-  , prescaleService_(0)
+HLTPrescaler::HLTPrescaler(edm::ParameterSet const& ps) :
+  b_(ps.getParameter<bool>("makeFilterObject")),
+  n_(ps.getParameter<unsigned int>("prescaleFactor")),
+  o_(ps.getParameter<unsigned int>("eventOffset")),
+  count_(0), 
+  ps_(0),
+  moduleLabel_(ps.getParameter<std::string>("@module_label"))
 {
-  if(edm::Service<edm::service::PrescaleService>().isAvailable())
-    prescaleService_ = edm::Service<edm::service::PrescaleService>().operator->();
-  else 
-    LogDebug("NoPrescaleService")<<"PrescaleService unavailable, prescaleFactor=1!";
-}
+  if (b_) produces<trigger::TriggerFilterObjectWithRefs>();
+  if (n_==0) n_=1; // accept all!
+  count_ = o_;     // event offset
 
-//______________________________________________________________________________    
+  // get prescale service
+  if(edm::Service<edm::service::PrescaleService>().isAvailable()) {
+    // isAvailable() throws only if the entire service system is not available
+    ps_ = edm::Service<edm::service::PrescaleService>().operator->();
+  } else {
+    LogDebug("HLTPrescaler ") << "non available service edm::service::PrescaleService.";
+    ps_ = 0;
+  }
+
+  if (ps_==0) {
+    LogDebug("HLTPrescaler ") << "prescale service pointer == 0 - using module config default.";
+  } else {
+    LogDebug("HLTPrescaler ") << "prescale service pointer != 0 - using prescale service.";
+  }
+
+}
+    
 HLTPrescaler::~HLTPrescaler()
 {
-  
 }
 
-
-////////////////////////////////////////////////////////////////////////////////
-// implementation of member functions
-////////////////////////////////////////////////////////////////////////////////
-
-//______________________________________________________________________________
-bool HLTPrescaler::beginLuminosityBlock(edm::LuminosityBlock & lb,
-					edm::EventSetup const& iSetup)
+bool HLTPrescaler::beginLuminosityBlock(edm::LuminosityBlock & lb, edm::EventSetup const& es)
 {
-  if (prescaleService_) {
-    unsigned int oldPrescale = prescaleFactor_;
-    prescaleFactor_ = prescaleService_->getPrescale(*pathName());
-    if (prescaleFactor_!=oldPrescale)
-      edm::LogInfo("ChangedPrescale")
-	<<"lumiBlockNb="<<lb.id().luminosityBlock()<<", "
-	<<"path="<<*pathName()<<": "<<prescaleFactor_<<" ["<<oldPrescale<<"]";
+  using namespace std;
+  using namespace edm;
+  using namespace reco;
+
+  LogDebug("HLTPrescaler") << "New LumiBlock: " <<lb.id().luminosityBlock();
+  if (ps_) {
+    // get prescale value from service 
+//  int newPrescale(ps_->getPrescale(lb.id().luminosityBlock(),moduleLabel_));
+    int newPrescale(ps_->getPrescale(moduleLabel_));
+    LogDebug("HLTPrescaler") << "Returned value: " << newPrescale;
+    if (newPrescale < 0 ) {
+      LogDebug("HLTPrescaler") << "PrescaleService: no info for module - using module value: " << n_ ;
+    } else {
+      n_=newPrescale;
+      if (n_==0) n_=1; // accept all!
+      count_ = o_;     // event offset
+    }
   }
+
   return true;
 }
 
-
-//______________________________________________________________________________
-bool HLTPrescaler::filter(edm::Event&, const edm::EventSetup&)
+bool HLTPrescaler::filter(edm::Event & e, const edm::EventSetup & es)
 {
-  ++eventCount_;
-  bool result = (prescaleFactor_==0) ? false : (eventCount_%prescaleFactor_==0);
-  if (result) acceptCount_++;
-  return result;
-}
+  using namespace std;
+  using namespace edm;
+  using namespace reco;
+  using namespace trigger;
 
+  // prescaler decision
+  ++count_;
+  const bool accept(count_%n_ == 0);
 
-//______________________________________________________________________________
-void HLTPrescaler::endJob()
-{
-  edm::LogInfo("PrescaleSummary")
-    <<acceptCount_<<"/"<<eventCount_
-    <<" ("<<100.*acceptCount_/(double)eventCount_<<"%) events accepted";
+  // construct and place filter object if requested
+  if (b_) {
+    auto_ptr<TriggerFilterObjectWithRefs> 
+      filterproduct (new TriggerFilterObjectWithRefs(path(),module()));
+    e.put(filterproduct);
+  }
+
+  return accept;
+
 }
